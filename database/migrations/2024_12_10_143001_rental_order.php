5d34ec5c694c75c2c0971ea7c06c8fe40ec12b84
Huy <giahuy171024@gmail.com>
2025-11-04 23:02:27 +0700

    [Fix] Xử lý code để chạy

--- database/migrations/2024_12_10_143001_rental_order.php
+++ database/migrations/2024_12_10_143001_rental_order.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('rental_orders', function (Blueprint $table) {
             $table->id('order_id'); // Khóa chính tự động tăng
-            $table->string('user_id'); // user_id
-            $table->unsignedBigInteger('rental_id'); // car_id
-            $table->enum('status', ['Pending', 'Deposit Paid', 'Paid', 'Canceled'])->default('Pending'); // Đặt giá trị mặc định là 'Pending'
-            $table->timestamp('order_date')->useCurrent(); // Ngày tạo đơn hàng, mặc định là thời gian hiện tại
-            $table->timestamps(); // Thời gian tạo và cập nhật
-    
+            $table->string('user_id'); // ID người dùng
+            $table->unsignedBigInteger('rental_id'); // ID xe thuê
+            $table->enum('status', ['Pending', 'Deposit Paid', 'Paid', 'Canceled'])->default('Pending');
+            $table->timestamp('order_date')->useCurrent();
+            $table->timestamps();
+
             // Khóa ngoại
             $table->foreign('user_id')->references('id')->on('accounts')->onDelete('cascade');
             $table->foreign('rental_id')->references('rental_id')->on('rental_cars')->onDelete('cascade');
@@ -32,7 +32,7 @@ return new class extends Migration
     {
         Schema::table('rental_orders', function (Blueprint $table) {
             $table->dropForeign(['user_id']);
-            $table->dropForeign(['car_id']);
+            $table->dropForeign(['rental_id']);
         });
 
         Schema::dropIfExists('rental_orders');
